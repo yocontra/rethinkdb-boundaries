5883467a0f2072e833511c006105dc059898237d
Contra <contra@maricopa.edu>
2016-05-29 14:24:38 -0400

    retry ftp connection, census bureau fails all the time

--- src/getFTP.js
+++ src/getFTP.js
@@ -1,15 +1,24 @@
 import FTPClient from 'ftp'
 import once from 'once'
 
-export default (opt, cb) => {
+const makeConnection = (opt, cb) => {
   cb = once(cb)
   const client = new FTPClient()
+  const retry = setTimeout(() => {
+    client.end()
+    makeConnection(opt, cb)
+  }, 5000)
 
   client.once('ready', () => {
     client.removeListener('error', cb)
+    clearTimeout(retry)
     cb(null, client)
   })
   client.once('error', cb)
 
   client.connect(opt)
+
+
 }
+
+export default makeConnection
